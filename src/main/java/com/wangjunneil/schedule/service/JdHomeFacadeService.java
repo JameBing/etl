19887abce2ede2562b7ebb60c1b1598f2aa0f9d5
yangyongbing <123456789>
2016-11-17 20:48:43 +0800

    base version

--- src/main/java/com/wangjunneil/schedule/service/JdHomeFacadeService.java
+++ src/main/java/com/wangjunneil/schedule/service/JdHomeFacadeService.java
@@ -1,18 +1,17 @@
 package com.wangjunneil.schedule.service;
 
+import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.wangjunneil.schedule.entity.jdhome.*;
 import com.wangjunneil.schedule.service.jdhome.JdHomeApiService;
 import com.wangjunneil.schedule.service.jdhome.JdHomeInnerService;
-import com.wangjunneil.schedule.utility.DateTimeUtil;
-import com.wangjunneil.schedule.utility.URL;
-import o2o.openplatform.sdk.util.SignUtils;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author yangyongbing
@@ -73,7 +72,117 @@ public class JdHomeFacadeService {
 
     //新增推送订单
     public String newOrder(String billId,String statusId,String timestamp)throws Exception{
-        String json  = jdHomeApiService.newOrder(billId,statusId,timestamp);
+        /*String json  = jdHomeApiService.newOrder(billId,statusId,timestamp);*/
+
+        String json = "{\n" +
+            "\t\"code\": \"0\",\n" +
+            "\t\"msg\": \"操作成功\",\n" +
+            "\t\"data\": {\n" +
+            "\t\t\"code\": \"0\",\n" +
+            "\t\t\"msg\": \"操作成功\",\n" +
+            "\t\t\"result\": {\n" +
+            "\t\t\t\"pageNo\": \"1\",\n" +
+            "\t\t\t\"pageSize\": \"20\",\n" +
+            "\t\t\t\"maxPageSize\": \"100\",\n" +
+            "\t\t\t\"totalCount\": \"100\",\n" +
+            "\t\t\t\"resultList\": [\n" +
+            "\t\t\t\t{\n" +
+            "\t\t\t\t\t\"orderId\": \"100001036354906\",\n" +
+            "\t\t\t\t\t\"srcOrderId\": \"100001036354906\",\n" +
+            "\t\t\t\t\t\"srcInnerType\": \"0\",\n" +
+            "\t\t\t\t\t\"srcInnerOrderId\": \"0\",\n" +
+            "\t\t\t\t\t\"orderType\": \"13000\",\n" +
+            "\t\t\t\t\t\"orderStatus\": \"20010\",\n" +
+            "\t\t\t\t\t\"orderStatusTime\": \"2016-07-09 18:34:07\",\n" +
+            "\t\t\t\t\t\"orderStartTime\": \"2016-07-09 15:40:30\",\n" +
+            "\t\t\t\t\t\"orderPurchaseTime\": \"2016-07-09 15:40:50\",\n" +
+            "\t\t\t\t\t\"orderAgingType\": \"12\",\n" +
+            "\t\t\t\t\t\"orderPreStartDeliveryTime\": \"2016-07-09 17:41:00\",\n" +
+            "\t\t\t\t\t\"orderPreEndDeliveryTime\": \"2016-07-09 17:41:00\",\n" +
+            "\t\t\t\t\t\"orderCancelTime\": \"2016-07-09 18:34:07\",\n" +
+            "\t\t\t\t\t\"orderCancelRemark\": \"其它\",\n" +
+            "\t\t\t\t\t\"orgCode\": \"71948\",\n" +
+            "\t\t\t\t\t\"buyerFullName\": \"王小明\",\n" +
+            "\t\t\t\t\t\"buyerFullAddress\": \"上海市徐汇区乐山路19号广元西路乐山路，乐山路19号\",\n" +
+            "\t\t\t\t\t\"buyerTelephone\": \"18816912316\",\n" +
+            "\t\t\t\t\t\"buyerMobile\": \"18816912316\",\n" +
+            "\t\t\t\t\t\"produceStationNo\": \"11000478\",\n" +
+            "\t\t\t\t\t\"produceStationName\": \"SH031永和大王-广元店\",\n" +
+            "\t\t\t\t\t\"produceStationNoIsv\": \"\",\n" +
+            "\t\t\t\t\t\"deliveryStationNo\": \"11000478\",\n" +
+            "\t\t\t\t\t\"deliveryStationName\": \"SH031永和大王-广元店\",\n" +
+            "\t\t\t\t\t\"deliveryCarrierNo\": \"9966\",\n" +
+            "\t\t\t\t\t\"deliveryCarrierName\": \"众包配送\",\n" +
+            "\t\t\t\t\t\"deliveryBillNo\": \"1000001\",\n" +
+            "\t\t\t\t\t\"deliveryPackageWeight\": \"0.10000000149011612\",\n" +
+            "\t\t\t\t\t\"deliveryConfirmTime\": \"2016-07-09 18:34:07\",\n" +
+            "\t\t\t\t\t\"orderPayType\": \"4\",\n" +
+            "\t\t\t\t\t\"orderTotalMoney\": \"2500\",\n" +
+            "\t\t\t\t\t\"orderDiscountMoney\": \"0\",\n" +
+            "\t\t\t\t\t\"orderFreightMoney\": \"300\",\n" +
+            "\t\t\t\t\t\"orderBuyerPayableMoney\": \"28\",\n" +
+            "\t\t\t\t\t\"packagingMoney\": \"300\",\n" +
+            "\t\t\t\t\t\"orderInvoiceOpenMark\": \"0\",\n" +
+            "\t\t\t\t\t\"adjustIsExists\": \"true\",\n" +
+            "\t\t\t\t\t\"adjustId\": \"0\",\n" +
+            "\t\t\t\t\t\"ts\": \"2016-07-09 18:34:07\",\n" +
+            "\t\t\t\t\t\"orderExtend\": {\n" +
+            "\t\t\t\t\t\t\"orderId\": \"100001036354906\",\n" +
+            "\t\t\t\t\t\t\"buyerCoordType\": \"2\",\n" +
+            "\t\t\t\t\t\t\"buyerLng\": \"\",\n" +
+            "\t\t\t\t\t\t\"buyerLat\": \"31.19627\",\n" +
+            "\t\t\t\t\t\t\"orderInvoiceType\": \"\",\n" +
+            "\t\t\t\t\t\t\"orderInvoiceTitle\": \"\",\n" +
+            "\t\t\t\t\t\t\"orderInvoiceContent\": \"\",\n" +
+            "\t\t\t\t\t\t\"orderBuyerRemark\": \"\",\n" +
+            "\t\t\t\t\t\t\"businessTag\": \"dj_new_cashier;dj_bld;picking_up;\",\n" +
+            "\t\t\t\t\t\t\"equipmentId\": \"\",\n" +
+            "\t\t\t\t\t\t\"buyerPoi\": \"东大街东里社区\",\n" +
+            "\t\t\t\t\t\t\"businessTagId\": \"\",\n" +
+            "\t\t\t\t\t\t\"ordererName\": \"\",\n" +
+            "\t\t\t\t\t\t\"ordererMobile\": \"\",\n" +
+            "\t\t\t\t\t\t\"artificerPortraitUrl\": \"\"\n" +
+            "\t\t\t\t\t},\n" +
+            "\t\t\t\t\t\"product\": [{\n" +
+            "\t\t\t\t\t\t\t\"id\": \"1\",\n" +
+            "\t\t\t\t\t\t\t\"orderId\": \"\",\n" +
+            "\t\t\t\t\t\t\t\"adjustId\": \"\",\n" +
+            "\t\t\t\t\t\t\t\"skuId\": \"\",\n" +
+            "\t\t\t\t\t\t\t\"skuName\": \"\",\n" +
+            "\t\t\t\t\t\t\t\"skuIdIsv\": \"\",\n" +
+            "\t\t\t\t\t\t\t\"skuSpuId\": \"2400\",\n" +
+            "\t\t\t\t\t\t\t\"skuJdPrice\": \"2400\",\n" +
+            "\t\t\t\t\t\t\t\"skuCount\": \"1\",\n" +
+            "\t\t\t\t\t\t\t\"isGift\": \"true\",\n" +
+            "\t\t\t\t\t\t\t\"adjustMode\": \"0\",\n" +
+            "\t\t\t\t\t\t\t\"upcCode\": \"2302480000005\",\n" +
+            "\t\t\t\t\t\t\t\"artificerId\": \"\",\n" +
+            "\t\t\t\t\t\t\t\"artificerName\": \"\",\n" +
+            "\t\t\t\t\t\t\t\"skuStorePrice\": \"690\",\n" +
+            "\t\t\t\t\t\t\t\"skuCostPrice\": \"590\",\n" +
+            "\t\t\t\t\t\t\t\"PromotionType\": \"1\",\n" +
+            "\t\t\t\t\t\t\t\"skuTaxRate\": \"\",\n" +
+            "\t\t\t\t\t\t\t\"promotionId\": \"140828\"\n" +
+            "\t\t\t\t\t\t}],\n" +
+            "\t\t\t\t\t\"orderDiscountList\": [\n" +
+            "\t\t\t\t\t\t{\n" +
+            "\t\t\t\t\t\t\t\"id\": \"1\",\n" +
+            "\t\t\t\t\t\t\t\"orderId\": \"100001036354906\",\n" +
+            "\t\t\t\t\t\t\t\"adjustId\": \"1000010248591821\",\n" +
+            "\t\t\t\t\t\t\t\"skuId\": \"2002445142\",\n" +
+            "\t\t\t\t\t\t\t\"skuIds\": \"2000830029,2002355309\",\n" +
+            "\t\t\t\t\t\t\t\"discountType\": \"1\",\n" +
+            "\t\t\t\t\t\t\t\"discountDetailType\": \"1\",\n" +
+            "\t\t\t\t\t\t\t\"discountCode\": \"H4a522\",\n" +
+            "\t\t\t\t\t\t\t\"discountPrice\": \"602\"\n" +
+            "\t\t\t\t\t\t}\n" +
+            "\t\t\t\t\t]\n" +
+            "\t\t\t\t}\n" +
+            "\t\t\t]\n" +
+            "\t\t},\n" +
+            "\t\t\"detail\": \"\"\n" +
+            "\t}\n" +
+            "}";
         JSONObject jsonObject =JSONObject.parseObject(json);
         if("0".equals(jsonObject.get("code"))){
             log.info("=====订单接口推送成功=====");
@@ -97,7 +206,7 @@ public class JdHomeFacadeService {
                 order.setOrderStartTime(jsonOrder.getDate("orderStatusTime"));
                 order.setOrderStartTime(jsonOrder.getDate("orderStartTime"));
                 order.setOrderPurchaseTime(jsonOrder.getDate("orderPurchaseTime"));
-                order.setOrderAgingType(jsonOrder.getInteger("orderPurchaseTime"));
+                order.setOrderAgingType(jsonOrder.getInteger("orderAgingType"));
                 order.setOrderPreStartDeliveryTime(jsonOrder.getDate("orderPreStartDeliveryTime"));
                 order.setOrderPreEndDeliveryTime(jsonOrder.getDate("orderPreEndDeliveryTime"));
                 order.setOrderCancelTime(jsonOrder.getDate("orderCancelTime"));
@@ -140,7 +249,7 @@ public class JdHomeFacadeService {
                 try {
                     jdHomeInnerService.addSyncOrders(orders);
                 }catch (Exception e){
-
+                    return "{\"code\":\"2\",\"msg\":\"failure\",\"data\":\"{}\"}";
                 }
                 log.info("=====MongoDb insert Order end====");
                 return "{\"code\":\"0\",\"msg\":\"success\",\"data\":\"{}\"}";
@@ -219,4 +328,11 @@ public class JdHomeFacadeService {
         }
         return discounts;
     }
+
+    //商家确认/取消接单接口
+    public String orderAcceptOperate(OrderAcceptOperate acceptOperate)throws Exception{
+        String json = jdHomeApiService.orderAcceptOperate(acceptOperate);
+        return json;
+    }
+
 }
