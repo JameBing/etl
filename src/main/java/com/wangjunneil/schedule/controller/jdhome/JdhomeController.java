3f25386854fc73de465a2bb578f9fa2f3f61c6fb
yangyongbing <123456789>
2016-11-17 11:36:16 +0800

    base version

---
+++ src/main/java/com/wangjunneil/schedule/controller/jdhome/JdhomeController.java
@@ -0,0 +1,117 @@
+package com.wangjunneil.schedule.controller.jdhome;
+
+
+import com.alibaba.fastjson.JSONObject;
+import com.wangjunneil.schedule.entity.jdhome.QueryStockRequest;
+import com.wangjunneil.schedule.entity.jdhome.ShopCategory;
+import com.wangjunneil.schedule.service.JdHomeFacadeService;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author yangyongbing
+ * @since 2016/11/15.
+ */
+@Controller
+@RequestMapping("/jdHome")
+public class JdHomeController {
+
+    private static Logger log = Logger.getLogger(JdHomeController.class.getName());
+
+    @Autowired
+    private JdHomeFacadeService jdHomeFacadeService;
+
+    /**
+     * 门店/歇业开业
+     * @param out
+     * @param req
+     * @param resp
+     * @return
+     */
+    @RequestMapping(value = "/openStore",method = RequestMethod.GET)
+    public String openStore(PrintWriter out,HttpServletRequest req, HttpServletResponse resp)throws Exception{
+        resp.setContentType("text/html;charset=utf-8");
+        //应用参数
+        List<QueryStockRequest> listBaseStockCenterRequest = new ArrayList<QueryStockRequest>();
+        QueryStockRequest queryStockRequest = new QueryStockRequest();
+        // 测试数据
+        queryStockRequest.setStationNo("10006172");
+        queryStockRequest.setSkuId(1997342L);
+        queryStockRequest.setDoSale(0);
+        listBaseStockCenterRequest.add(queryStockRequest);
+        String returnJson = jdHomeFacadeService.updateAllStockOn(listBaseStockCenterRequest);
+        out.println(returnJson);
+        out.close();
+        return  null;
+    }
+
+    /**
+     * 新增商品分类
+     * @param out
+     * @param req
+     * @param resp
+     * @return
+     */
+    @RequestMapping(value = "/addShopCategory", method=RequestMethod.GET)
+    public String addShopCategory(PrintWriter out,HttpServletRequest req, HttpServletResponse resp)throws Exception{
+        resp.setContentType("text/html;charset=utf-8");
+        ShopCategory shopCategory = new ShopCategory();
+        shopCategory.setPid(000L);
+        shopCategory.setShopCategoryLevel(0);
+       // shopCategory.setCreatePin("aaa");//
+        shopCategory.setSort(1);
+        shopCategory.setShopCategoryName("bb");
+        String returnJson = jdHomeFacadeService.addShopCategory(shopCategory);
+        out.println(returnJson);
+        out.close();
+        return  null;
+    }
+
+    @RequestMapping(value = "/updateShopCategory", method=RequestMethod.GET)
+    public String updateShopCategory(PrintWriter out,HttpServletRequest req, HttpServletResponse resp)throws Exception{
+        resp.setContentType("text/html;charset=utf-8");
+        ShopCategory shopCategory = new ShopCategory();
+        shopCategory.setId(111L);
+        shopCategory.setShopCategoryName("cc");
+        String returnJson = jdHomeFacadeService.updateShopCategory(shopCategory);
+        out.println(returnJson);
+        out.close();
+        return  null;
+    }
+
+    @RequestMapping(value = "/deleteShopCategory", method=RequestMethod.GET)
+    public String deleteShopCategory(PrintWriter out,HttpServletRequest req, HttpServletResponse resp)throws Exception{
+        resp.setContentType("text/html;charset=utf-8");
+        ShopCategory shopCategory = new ShopCategory();
+        shopCategory.setId(111L);
+        String returnJson = jdHomeFacadeService.deleteShopCategory(shopCategory);
+        out.println(returnJson);
+        out.close();
+        return  null;
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/djsw/newOrder",method = RequestMethod.GET)
+    public JSONObject newOrder(@RequestBody JSONObject jsonObject)throws Exception{
+        if(jsonObject == null){
+            return jsonObject;
+        }
+        JSONObject json = jsonObject.getJSONObject("jd_param_json");
+        String billId = json.getString("billId");
+        String statusId = json.getString("statusId");
+        String timestamp = json.getString("timestamp");
+        String rtnJson = jdHomeFacadeService.newOrder(billId,statusId,timestamp);
+        return  null;
+    }
+}
