325652c38817f026722f1d84865738725dd3e720
LiuSirs <Lxxnp@foxmail.com>
2016-11-21 18:13:45 +0800

    baseversion

---
+++ src/main/java/com/wangjunneil/schedule/controller/jdhome/JdHomeController.java
@@ -0,0 +1,7 @@
+package com.wangjunneil.schedule.controller.jdhome;
+
+/**
+ * Created by Administrator on 2016-11-17.
+ */
+public class JdHomeController {
+}
---
+++ src/main/java/com/wangjunneil/schedule/controller/mt/MtController.java
@@ -0,0 +1,7 @@
+package com.wangjunneil.schedule.controller.mt;
+
+/**
+ * Created by Administrator on 2016-11-17.
+ */
+public class MtController {
+}
