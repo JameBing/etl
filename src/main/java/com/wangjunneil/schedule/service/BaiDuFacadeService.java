c02e8ec6362dd9e720212cadfe950b30cec52437
yuxiaowei <yuxiaowei@ziyanfoods.com>
2016-12-08 20:48:39 +0800

    base version

--- src/main/java/com/wangjunneil/schedule/service/BaiDuFacadeService.java
+++ src/main/java/com/wangjunneil/schedule/service/BaiDuFacadeService.java
@@ -168,7 +168,7 @@ public class BaiDuFacadeService {
              log = sysFacadeService.functionRtn.apply(ex);
         }
         catch (Exception ex) {
-            sysFacadeService.functionRtn.apply(ex);
+            log =  sysFacadeService.functionRtn.apply(ex);
             rtn.setCode(-998);
         }
         finally {
@@ -309,106 +309,6 @@ public class BaiDuFacadeService {
         result = gson1.toJson(rtn);
         return result;
     }
-
-//    //接收百度外卖推送过来的订单(2.0)
-//    public String orderPost(String params) throws BaiDuException{
-//        String requestStr = "{\n" +
-//            "    \"body\": {\n" +
-//            "        \"shop\": {\n" +
-//            "            \"id\": \"2520\",\n" +
-//            "            \"name\": \"百度外卖（测试店5555）\"\n" +
-//            "        },\n" +
-//            "        \"order\": {\n" +
-//            "            \"order_id\": \"14347012309352\",\n" +
-//            "            \"send_immediately\": 1,\n" +
-//            "            \"send_time\": \"1\",\n" +
-//            "            \"send_fee\": 500,\n" +
-//            "            \"package_fee\": 200,\n" +
-//            "            \"discount_fee\": 0,\n" +
-//            "            \"total_fee\": 3700,\n" +
-//            "            \"shop_fee\": 3200,\n" +
-//            "            \"user_fee\": 3700,\n" +
-//            "            \"pay_type\": 1,\n" +
-//            "            \"pay_status\": 1,\n" +
-//            "            \"need_invoice\": 2,\n" +
-//            "            \"invoice_title\": \"\",\n" +
-//            "            \"remark\": \"请提供餐具\",\n" +
-//            "            \"delivery_party\": 1,\n" +
-//            "            \"create_time\": \"1434701230\"\n" +
-//            "        },\n" +
-//            "        \"user\": {\n" +
-//            "            \"name\": \"测试订单请勿操作\",\n" +
-//            "            \"phone\": \"18912345678\",\n" +
-//            "            \"gender\": 1,\n" +
-//            "            \"address\": \"北京海淀区奎科科技大厦 测试\",\n" +
-//            "            \"coord\": {\n" +
-//            "                \"longitude\": 116.143145,\n" +
-//            "                \"latitude\": 39.741426\n" +
-//            "            }\n" +
-//            "        },\n" +
-//            "        \"products\": [\n" +
-//            "            {\n" +
-//            "                \"product_id\": \"12277320\",\n" +
-//            "                \"product_name\": \"酱肉包（/份）\",\n" +
-//            "                \"product_price\": 1200,\n" +
-//            "                \"product_amount\": 1,\n" +
-//            "                \"product_fee\": 1200,\n" +
-//            "                \"package_price\": 100,\n" +
-//            "                \"package_amount\": 1,\n" +
-//            "                \"package_fee\": 100,\n" +
-//            "                \"total_fee\": 1300,\n" +
-//            "                \"upc\": \"\"\n" +
-//            "            }\n" +
-//            "        ],\n" +
-//            "        \"discount\": []\n" +
-//            "    },\n" +
-//            "    \"cmd\": \"order.create\",\n" +
-//            "    \"sign\": \"E362B8AACE4F7A77047A885C0C0D230D\",\n" +
-//            "    \"source\": \"65400\",\n" +
-//            "    \"ticket\": \"909C3B92-8CDD-AF2C-C887-5B660233E2B2\",\n" +
-//            "    \"timestamp\": 1434701234,\n" +
-//            "    \"version\": \"2.0\"\n" +
-//            "}";
-//        String orderStr = "{\n" +
-//            "            \"order_id\": \"14347012309352\",\n" +
-//            "            \"send_immediately\": 1,\n" +
-//            "            \"send_time\": \"1\",\n" +
-//            "            \"send_fee\": 500,\n" +
-//            "            \"package_fee\": 200,\n" +
-//            "            \"discount_fee\": 0,\n" +
-//            "            \"total_fee\": 3700,\n" +
-//            "            \"shop_fee\": 3200,\n" +
-//            "            \"user_fee\": 3700,\n" +
-//            "            \"pay_type\": 1,\n" +
-//            "            \"pay_status\": 1,\n" +
-//            "            \"need_invoice\": 2,\n" +
-//            "            \"invoice_title\": \"\",\n" +
-//            "            \"remark\": \"请提供餐具\",\n" +
-//            "            \"delivery_party\": 1,\n" +
-//            "            \"create_time\": \"1434701230\"\n" +
-//            "        }";
-//        String userStr =" {\n" +
-//            "            \"name\": \"测试订单请勿操作\",\n" +
-//            "            \"phone\": \"18912345678\",\n" +
-//            "            \"gender\": 1,\n" +
-//            "            \"address\": \"北京海淀区奎科科技大厦 测试\",\n" +
-//            "            \"coord\": {\n" +
-//            "                \"longitude\": 116.143145,\n" +
-//            "                \"latitude\": 39.741426\n" +
-//            "            }\n" +
-//            "        }";
-//        requestStr.replace("/", "\\/");
-//        SysParams sysParams = new SysParams();
-//        try {
-//            sysParams = getGson().fromJson(requestStr, SysParams.class);
-//            Body body = getGson().fromJson(getGson().toJson(sysParams.getBody()).toString(), Body.class);
-//            //将订单信息插入Mongodb
-//            baiDuInnerService.updSyncBaiDuOrder(body);
-//        }catch (Exception ex){
-//            throw new BaiDuException("",sysParams,ex);  //异常这块待增强，考虑结构化的异常信息，引入日志ID号，用于返回给Pos方
-//        }
-//        return  null;
-//    }
 
     //接收百度外卖推送过来的订单
     public String orderPost(SysParams sysParams){
